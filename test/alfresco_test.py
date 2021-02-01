28d1b0baa0a017b02eb52bf08cb4b594efcb04aa
Alvaro Figueroa <alvaro@greencore.co.cr>
2021-02-01 09:48:41 -0600

    TLS cert ignore for tests

--- test/alfresco_test.py
+++ test/alfresco_test.py
@@ -4,32 +4,32 @@ import requests
 class ShareTest(unittest.TestCase):
 
     def test_share_avl(self):
-        r = requests.get('http://localhost/share/')
+        r = requests.get('http://localhost/share/', verify=False)
         r.status_code
         self.assertEqual(r.status_code, 200)
 
     def test_alf_avl(self):
-        r = requests.get('http://localhost/alfresco/')
+        r = requests.get('http://localhost/alfresco/', verify=False)
         r.status_code
         self.assertEqual(r.status_code, 200)
 
     def test_share_redir_avl(self):
-        r = requests.get('http://localhost/nonexistent')
+        r = requests.get('http://localhost/nonexistent', verify=False)
         r.status_code
         self.assertEqual(r.status_code, 200)
 
     def test_fail_avl(self):
-        r = requests.get('http://localhost/share/nonexistent')
+        r = requests.get('http://localhost/share/nonexistent', verify=False)
         r.status_code
         self.assertEqual(r.status_code, 404)
 
     def test_share_login(self):
-        r = requests.get('http://localhost/alfresco/service/index', auth=('admin', 'admin'))
+        r = requests.get('http://localhost/alfresco/service/index', auth=('admin', 'admin'), verify=False)
         r.status_code
         self.assertEqual(r.status_code, 200)
 
     def test_share_login_fail(self):
-        r = requests.get('http://localhost/alfresco/service/index', auth=('admin', 'notadmin'))
+        r = requests.get('http://localhost/alfresco/service/index', auth=('admin', 'notadmin'), verify=False)
         r.status_code
         self.assertEqual(r.status_code, 401)
 
