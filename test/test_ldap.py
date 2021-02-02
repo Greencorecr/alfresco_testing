17715be9f614e13b37bdf1611520a95054a89ce4
Alvaro Figueroa <alvaro@greencore.co.cr>
2021-02-02 08:54:45 -0600

    Testing ldap on CI

--- test/test_ldap.py
+++ test/test_ldap.py
@@ -1,12 +1,18 @@
 import unittest
 import requests
 
-class AuditTest(unittest.TestCase):
+class LdapTest(unittest.TestCase):
 
-    def test_audit(self):
-        r = requests.get('https://localhost/alfresco/service/api/audit/control/', auth=('ldapuser', 'greencore123'), verify=False)
+    def test_ldap(self):
+        r = requests.get('https://localhost/alfresco/service/index', auth=('ldapuser', 'dogood'), verify=Fals
+e)
         r.status_code
         self.assertEqual(r.status_code, 200)
+
+    def test_ldap_login_fail(self):
+        r = requests.get('https://localhost/alfresco/service/index', auth=('nouser', 'wrongpass'), verify=False)
+        r.status_code
+        self.assertEqual(r.status_code, 401)
 
 if __name__ == '__main__':
     unittest.main()
